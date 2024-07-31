a951eba6bf298d51e9d51fe681d64b6a7b959c18
habibkoe <habibsantri@gmail.com>
2024-07-31 22:31:07 +0800

    config build at package.json + vite

---
+++ src/index.tsx
@@ -0,0 +1,1 @@
+export { default as MakyoSelectWithSearch } from './components/SelectWithSearchCustom';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      outDir: 'dist',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'MakyoSelectWithSearch',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  }
 })
